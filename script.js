19e6e56b105ebc4122d18107880bb72804d3b915
rawgosh <rawgoshshrestha129@gmail.com>
2024-04-22 13:12:48 +0545

    basics of js

---
+++ script.js
@@ -0,0 +1,11 @@
+let character = 'Hello';
+
+/*console.log(character);   shows output in the console 
+character = 'World';        re-initializing the variable
+console.log(character);     
+let secondCharacter;         declaring a variable using camel case
+secondCharacter = "Test";
+console.log(secondCharacter);*/
+
+let count = 8;
+console.log(count + 1);
