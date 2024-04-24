e3274a81767508e13b2714bd74713e42d8d42a58
rawgosh <rawgoshshrestha129@gmail.com>
2024-04-24 13:41:39 +0545

    project started

---
+++ project.js
@@ -0,0 +1,20 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+/*for (let i = 0; i < count; i++) {
+    rows.push(padRow(i + 1, count));
+}*/
+
+
+let result = "";
+
+for (const row of rows) {
+    result +="\n" + row; //e;ements in a newline
+}
+
+console.log(result);
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let character = 'Hello';
+console.log(character);
 
 /*console.log(character);   shows output in the console 
 character = 'World';        re-initializing the variable
@@ -9,3 +10,16 @@ console.log(secondCharacter);*/
 
 let count = 8;
 console.log(count + 1);
+
+let rows = ["Naomi", "Quincy", "CamperChan"]; 
+rows[2] = 10;
+
+let pushed = rows.push("freeCodeCamp"); // inputs an element at the end of an array
+console.log(pushed); // returns the new length of the array after pushing the element
+console.log(rows);
+
+let length = rows[rows.length - 1];
+console.log(length); // to display the last element of an array
+
+let popped = rows.pop(); // removes the last element from an array and returns it
+console.log(popped);
