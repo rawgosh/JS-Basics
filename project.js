aea3c61e77c6d340e9341bf69cdf23a8ee64132e
rawgosh <rawgoshshrestha129@gmail.com>
2024-04-28 13:22:58 +0545

    90%

--- project.js
+++ project.js
@@ -6,10 +6,20 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+// FOR LOOP
 /*for (let i = 0; i < count; i++) {
     rows.push(padRow(i + 1, count));
 }*/
 
+// WHILE LOOP
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+//FOR LOOP TO INVERT
+/*for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}*/
 
 let result = "";
 
